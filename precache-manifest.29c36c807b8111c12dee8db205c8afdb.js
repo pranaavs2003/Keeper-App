f936765e3ba6352f7bacee1866569f0398522503
Pranaav S <pranaavs2003@gmail.com>
2022-05-30 21:20:38 +0530

    Updates

---
+++ precache-manifest.29c36c807b8111c12dee8db205c8afdb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc13818c0a66c41678c7f8d4d529c1e7",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/Keeper-App/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "684c366cb8f76073619e",
+    "url": "/Keeper-App/static/js/main.8f1d05b1.chunk.js"
+  },
+  {
+    "revision": "8ac763be5021583d99b9",
+    "url": "/Keeper-App/static/js/runtime-main.7fdd9867.js"
+  }
+]);
